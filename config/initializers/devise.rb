d51bf4a2b3ac9b31a82cbc33863211184a80760f
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-11-11 14:24:05 +0100

    s/metikular.ch/wort.schule/

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -38,7 +38,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "noreply@metikular.ch"
+  config.mailer_sender = "noreply@wort.schule"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
