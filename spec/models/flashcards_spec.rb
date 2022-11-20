d26824dba980fe7c25bf3f4dbc0aa21eb7581585
Serge Hänni <serge@nyi.ch>
2022-11-20 16:44:49 +0100

    Add removal of words from flashcards
    [#16]

--- spec/models/flashcards_spec.rb
+++ spec/models/flashcards_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "flashcards" do
     before do
       list1.words << [noun1, noun2]
       list2.words << [noun1]
+      learning_group.lists << [list1, list2]
       LearningGroupMembership.create!(learning_group:, student:, access: :granted)
     end
 
@@ -37,6 +38,24 @@ RSpec.describe "flashcards" do
       Flashcards.add_list(learning_group, list1)
       expect(student.first_flashcard_list.words).to match_array [noun2]
       expect(student.flashcard_list(Flashcards::SECTIONS.second).words).to match_array [noun1]
+    end
+
+    it "removes a word from the first section" do
+      expect(student.first_flashcard_list.words).to be_empty
+
+      Flashcards.add_list(learning_group, list1)
+      Flashcards.add_list(learning_group, list2)
+      expect(student.first_flashcard_list.words).to match_array [noun1, noun2]
+
+      # Do not remove word here, because it is still present on list2
+      list1.words.delete(noun1)
+      Flashcards.remove_word(list1, noun1)
+      expect(student.first_flashcard_list.words).to match_array [noun1, noun2]
+
+      # Now it is removed everywhere
+      list2.words.delete(noun1)
+      Flashcards.remove_word(list2, noun1)
+      expect(student.first_flashcard_list.words).to match_array [noun2]
     end
   end
 end
