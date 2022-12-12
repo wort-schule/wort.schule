7e0d3a457677d9019c9fec5e5f416bfb63a969f4
phortx <bk@itws.de>
2022-12-12 20:47:10 +0100

    fix: add psql extension for uuid

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_12_082227) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
