171b61691a7d3877c079869d2586841fc6ef5c57
Serge Hänni <serge@nyi.ch>
2022-12-16 18:29:33 +0100

    Add paper trail for schools
    [#106]

--- app/helpers/version_helper.rb
+++ app/helpers/version_helper.rb
@@ -9,8 +9,8 @@ module VersionHelper
     user = User.find_by(id: version.whodunnit)
     username = user&.full_name.presence || user&.email || I18n.t("shared.unknown")
     datetime = I18n.l version.created_at
-    action = I18n.t("words.actions.#{version.event}")
+    action = I18n.t("shared.versions.actions.#{version.event}")
 
-    I18n.t("words.versions.changed_by", action:, username:, datetime:)
+    I18n.t("shared.versions.changed_by", action:, username:, datetime:)
   end
 end
