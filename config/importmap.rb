abc286cea892c1ed07a13c9ee5a6dca3e5fda71c
Serge Hänni <serge@nyi.ch>
2023-02-23 14:46:20 +0100

    Remove rails-ujs and replace confirmation dialogs with Turbo

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/d
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-reveal-controller", to: "https://ga.jspm.io/npm:stimulus-reveal-controller@4.0.0/dist/stimulus-reveal-controller.es.js"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.0.5/lib/assets/compiled/rails-ujs.js"
 pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.0.3/dist/js/tom-select.complete.js"
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.6/src/index.js"
 pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@3.0.4/dist/tailwindcss-stimulus-components.modern.js"
