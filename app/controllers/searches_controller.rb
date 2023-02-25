a16a48c93eacbee96e49940c03db05d49ebff93e
Serge Hänni <serge@nyi.ch>
2023-02-25 12:09:49 +0100

    Add result count to word types
    [#171]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,5 +13,22 @@ class SearchesController < PublicController
       .find
       .ordered_lexigraphically
       .page(params[:page])
+
+    @counts = {
+      all: @words.total_count,
+      nouns: count_for("Noun"),
+      verbs: count_for("Verb"),
+      adjectives: count_for("Adjective"),
+      function_words: count_for("FunctionWord")
+    }
+  end
+
+  private
+
+  def count_for(word_type)
+    initialize_filterrific(
+      Word,
+      (params[:filterrific] || {}).merge(filter_type: word_type)
+    ).find.count
   end
 end
