c23dde59322c689b7a4ce0aaced4bea797d0406b
phortx <bk@itws.de>
2023-02-27 17:13:46 +0100

    feat: better splitting

--- app/controllers/seo_controller.rb
+++ app/controllers/seo_controller.rb
@@ -1,6 +1,49 @@
 class SeoController < PublicController
   def word_index
     @letter = params[:letter] || "a"
-    @words = Word.where("name ILIKE ?", "#{@letter}%").order("LOWER(name)").page(params[:page])
+    @words = words_with_variants
+  end
+
+  # Returns all words in the database that start with the given letter, including all variants like plural,
+  # comparative, etc. and sorted by the word itself.
+  private def words_with_variants
+    # TODO: case_X singular and plural?
+    %i[
+      name
+      plural comparative superlative
+      imperative_singular imperative_plural participle past_participle
+      present_singular_1 present_singular_2 present_singular_3 present_plural_1 present_plural_2 present_plural_3
+      past_singular_1 past_singular_2 past_singular_3 past_plural_1 past_plural_2 past_plural_3
+    ]
+      .map { |field| Word.where("#{field} ILIKE ?", "#{@letter}%").map { |w| build_word_hash(w, field) } }
+      .flatten
+      .sort_by { |w| w[:label].downcase }
+  end
+
+  # Generates a simple hash for the given word and field as a data object for the view.
+  private def build_word_hash(word, field)
+    suffix = if field == :plural
+      ", die"
+    elsif word.is_a?(Noun) && field == :name
+      ", #{word.article_definite(case_number: 1, singular: true)}"
+    else
+      ""
+    end
+
+    {
+      label: word.send(field) + suffix,
+      word_type_label: word_type_label(word, field),
+      base_record: word
+    }
+  end
+
+  # TODO: Better translations
+  # Generates a human readable label for the given word type.
+  private def word_type_label(word, field)
+    if field == :name
+      t("activerecord.models.#{word.class.name.downcase}.one")
+    else
+      t("theme.descriptions.#{field}_html") + " von #{word.name}"
+    end
   end
 end
--- spec/features/seo_spec.rb
+++ spec/features/seo_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe "SEO" do
 
     it "lists all words for the respective letter" do
       visit word_index_path(letter: "a")
-      expect(page).to have_selector "li", text: "#{noun.name}, #{noun.article_definite(case_number: 1, singular: true)} - #{noun.plural}, #{noun.article_definite(case_number: 1, singular: false)}"
+      expect(page).to have_selector "li", text: "#{noun.name}, #{noun.article_definite(case_number: 1, singular: true)}"
+      expect(page).to have_selector "li", text: "#{noun.plural}, #{noun.article_definite(case_number: 1, singular: false)}"
 
       visit word_index_path(letter: "b")
-      expect(page).to have_selector "li", text: "#{adjective.name}, #{adjective.comparative}, #{adjective.superlative}"
+      expect(page).to have_selector "li", text: adjective.name.to_s
+      expect(page).to have_selector "li", text: adjective.comparative.to_s
+      expect(page).to have_selector "li", text: adjective.superlative.to_s
 
       visit word_index_path(letter: "d")
-      expect(page).to have_selector "li", text: "#{verb.name}, #{verb.past_singular_1}"
+      expect(page).to have_selector "li", text: verb.name.to_s
+      expect(page).to have_selector "li", text: verb.past_singular_1.to_s
     end
   end
 end
