4b58eec0bbc465835df363e513685193eeebf7a3
Serge Hänni <serge@nyi.ch>
2023-03-02 18:03:54 +0100

    Change logo not to be hidden on mobile
    [#229]

--- app/javascript/controllers/home_search_controller.js
+++ app/javascript/controllers/home_search_controller.js
@@ -2,34 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="home-search"
 export default class extends Controller {
-  static targets = [
-    "input",
-    "logo"
-  ]
+  static targets = ["input"]
 
   static values = {
     path: String
   }
 
-  connect() {
-    this.updateLogoVisibility(false)
-  }
-
   input(event) {
     if (event.key === 'Enter') {
       this.goToSearchResults()
-      return
     }
-
-    this.updateLogoVisibility(true)
-  }
-
-  updateLogoVisibility(reposition) {
-    if(window.innerWidth > 768) return
-
-    var hasQuery = this.inputTarget.value.length > 0
-    this.logoTarget.style.display = hasQuery ? 'none' : 'block'
-    if(reposition) this.inputTarget.scrollIntoView({ behavior: 'smooth' })
   }
 
   goToSearchResults() {
