87a41a697331c3858e207531f74a0f2471eb0da1
Serge Hänni <serge@nyi.ch>
2023-03-22 10:53:22 +0100

    Add support for 3 column box
    [#276]

--- app/components/box_grid_component.rb
+++ app/components/box_grid_component.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class BoxGridComponent < ViewComponent::Base
+  attr_reader :columns_count
+
+  def initialize(columns: 2)
+    @columns_count = columns
+  end
+
+  def grid_class
+    case columns_count
+    when 3 then "grid-cols-3"
+    else "grid-cols-2"
+    end
+  end
 end
