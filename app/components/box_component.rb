ec460d40e2acdb488e4a68477b33dc142e4fb693
Serge Hänni <serge@nyi.ch>
2023-03-22 12:48:09 +0100

    Add 'Versionsänderungen' box
    [#276]

--- app/components/box_component.rb
+++ app/components/box_component.rb
@@ -3,7 +3,16 @@
 class BoxComponent < ViewComponent::Base
   attr_reader :title
 
-  def initialize(title:)
+  def initialize(title:, span: 1)
     @title = title
+    @span = span
+  end
+
+  def span_class
+    case @span
+    when 1 then ""
+    when 2 then "col-span-2"
+    else ""
+    end
   end
 end
