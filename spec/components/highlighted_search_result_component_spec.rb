ab527ea4aa2d8907cac4d8296772fdc3c79f841e
Serge Hänni <serge@nyi.ch>
2023-04-03 21:45:58 +0200

    Add more sophisticated display of search hits
    [#289]

--- spec/components/highlighted_search_result_component_spec.rb
+++ spec/components/highlighted_search_result_component_spec.rb
@@ -5,16 +5,18 @@ require "rails_helper"
 RSpec.describe HighlightedSearchResultComponent, type: :component do
   it "splits results correctly" do
     # Start
-    expect(described_class.new(result: "Haus", query: "ha").parts).to eq %w[Ha us]
+    expect(described_class.new(result: Word.new(name: "Haus"), query: "ha").parts).to eq %w[Ha us]
     # Middle
-    expect(described_class.new(result: "Bauhaus", query: "ha").parts).to eq %w[Bau ha us]
+    expect(described_class.new(result: Word.new(name: "Bauhaus"), query: "ha").parts).to eq %w[Bau ha us]
     # End
-    expect(described_class.new(result: "Bauhaus", query: "aus").parts).to eq %w[Bauh aus]
+    expect(described_class.new(result: Word.new(name: "Bauhaus"), query: "aus").parts).to eq %w[Bauh aus]
     # Multiple
-    expect(described_class.new(result: "Bauhaus", query: "au").parts).to eq %w[B au h au s]
+    expect(described_class.new(result: Word.new(name: "Bauhaus"), query: "au").parts).to eq %w[B au h au s]
     # Complete word
-    expect(described_class.new(result: "Haus", query: "haus").parts).to eq %w[Haus]
+    expect(described_class.new(result: Word.new(name: "Haus"), query: "haus").parts).to eq %w[Haus]
     # Regex
-    expect(described_class.new(result: "Haus", query: "h.").parts).to eq %w[Haus]
+    expect(described_class.new(result: Word.new(name: "Haus"), query: "h.").parts).to eq %w[Haus]
+    # Noun with articlej
+    expect(described_class.new(result: Noun.new(name: "Haus", genus_id: 2), query: "hau").parts).to eq ["das&nbsp;", "Hau", "s"]
   end
 end
