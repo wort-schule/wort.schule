f1c0ec423690b8a17bc4b39891b60ecd80491105
Serge Hänni <serge@nyi.ch>
2023-06-15 10:49:35 +0200

    Add adding/removal of words when changing users in learning group
    [#317]

--- app/models/learning_group_membership.rb
+++ app/models/learning_group_membership.rb
@@ -8,4 +8,12 @@ class LearningGroupMembership < ApplicationRecord
   enumerize :role, in: %i[member group_admin], scope: true
 
   validates :user, uniqueness: {scope: :learning_group_id}
+
+  after_destroy :remove_words
+
+  private
+
+  def remove_words
+    Flashcards.remove_user(learning_group, user)
+  end
 end
