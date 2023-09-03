287cbc7bf3263c69ebd841d24e221f5da0c1c4c7
Serge Hänni <serge@nyi.ch>
2023-09-03 20:38:02 +0200

    Change params to be removed depending on search mode
    [#116]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,9 +6,10 @@ class SearchesController < PublicController
 
   def show
     @filter_type = params.dig(:filterrific, :filter_type).presence || ""
+    @mode = FilterMode.new(mode: params[:mode])
     @filterrific = initialize_filterrific(
       Word,
-      (params[:filterrific] || {}).merge(filter_type: @filter_type)
+      (filter_params || {}).merge(filter_type: @filter_type)
     ) or return
 
     @words = @filterrific
@@ -23,8 +24,6 @@ class SearchesController < PublicController
       adjectives: word_count_for("Adjective"),
       function_words: word_count_for("FunctionWord")
     }
-
-    @mode = FilterMode.new(mode: params[:mode])
   end
 
   private
@@ -35,5 +34,55 @@ class SearchesController < PublicController
 
   def full_width?
     true
+  end
+
+  def filter_params
+    allowed_params = params.fetch(:filterrific, {}).permit!.select { |filter| permitted_filters.map(&:to_s).include?(filter) }
+    params[:filterrific] = allowed_params
+
+    allowed_params
+  end
+
+  def permitted_filters
+    always_permitted = %i[filter_type filter_home]
+    advanced_filters = %i[
+      filter_wordstarts
+      filter_wordends
+      filter_wordcontains
+      filter_syllablescontains
+      filter_topic
+      filter_hierarchy
+      filter_keywords
+    ]
+    power_filters = %i[
+      filter_letters
+      filter_source
+      filter_consonant_vowel
+      filter_phenomenons
+      filter_strategies
+      filter_foreign
+      filter_modal
+      filter_compound
+      filter_example_sentences
+      filter_singularetantum
+      filter_pluraletantum
+      filter_prototype
+      filter_subjectless
+      filter_perfect_haben
+      filter_perfect_sein
+      filter_strong
+      filter_irregular_declination
+      filter_absolute
+      filter_irregular_comparison
+    ]
+
+    case @mode.mode
+    when :omni
+      always_permitted
+    when :advanced
+      always_permitted + advanced_filters
+    when :power
+      always_permitted + advanced_filters + power_filters
+    end
   end
 end
