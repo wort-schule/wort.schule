eea2cf5d856bff46ebc96b1dd472e869a604c31e
Serge Hänni <serge@nyi.ch>
2024-05-31 12:56:17 +0200

    Add montessori symbols
    [#113]

--- app/components/word_header_component.rb
+++ app/components/word_header_component.rb
@@ -9,4 +9,20 @@ class WordHeaderComponent < ViewComponent::Base
   def initialize(word:)
     @word = word
   end
+
+  def montessori_symbol
+    case word.type
+    when "Noun" then "montessori/nomen.svg"
+    when "Verb" then "montessori/verb.svg"
+    when "Adjective" then "montessori/adjektiv.svg"
+    when "FunctionWord"
+      case word.function_type
+      when "article_definite", "article_indefinite" then "montessori/artikel.svg"
+      when "auxiliary_verb" then "montessori/hilfsverb.svg"
+      when "conjunction" then "montessori/konjunktion.svg"
+      when "preposition" then "montessori/praeposition.svg"
+      when "pronoun" then "montessori/pronomen.svg"
+      end
+    end
+  end
 end
