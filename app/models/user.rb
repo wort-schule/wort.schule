96eeaaac96e42d0461d5f8abb40d1761f3b94fd6
Serge Hänni <serge@nyi.ch>
2024-05-31 16:01:39 +0200

    Add special font rendering
    [#440]

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,6 +62,10 @@ class User < ApplicationRecord
     membership.access.granted? && !generated_account?
   end
 
+  def word_font
+    Fonts::AVAILABLE_FONTS.first
+  end
+
   private
 
   def setup_flashcards
---
+++ app/services/fonts.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Fonts
+  AVAILABLE_FONTS = [
+    {name: "Druckschrift Buch", filename: "DRBuch"},
+    {name: "Druckschrift Hand", filename: "DRHand"},
+    {name: "Grundschrift", filename: "GS"},
+    {name: "Lateinische Ausgangsschrift", filename: "LA"},
+    {name: "Schulausgangsschrift", filename: "SAS"},
+    {name: "Vereinfachte Ausgangsschrift", filename: "VA"}
+  ]
+end
