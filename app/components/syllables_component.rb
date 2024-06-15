06b07b916ab71779e594c636c7526aafe35b63f1
Serge Hänni <serge@nyi.ch>
2024-06-15 16:24:54 +0200

    Move logic from view to component
    [#440]

--- app/components/syllables_component.rb
+++ app/components/syllables_component.rb
@@ -7,6 +7,16 @@ class SyllablesComponent < ViewComponent::Base
     @syllables = text.split("-")
   end
 
+  def syllables_with_arcs
+    syllables.map do |syllable|
+      arc = syllable_arc.arc(syllable)
+
+      "#{arc}#{syllable}"
+    end
+  end
+
+  private
+
   def syllable_arc
     @syllable_arc ||= SyllableArc.new(Rails.root.join("app/assets/fonts/#{helpers.current_font}-Regular.ttf"))
   end
