a95ccf0db67da4821bae5e7c3b7e9ab406ba3986
Serge Hänni <serge@nyi.ch>
2024-06-16 18:01:35 +0200

    Add different wording for word types and genus
    [#440]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_16_133652) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_152222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pgcrypto"
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_133652) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genus_keys", default: [], array: true
   end
 
   create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -395,6 +396,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_133652) do
     t.boolean "show_montessori_symbols", default: true, null: false
     t.boolean "show_fresch_symbols", default: true, null: false
     t.boolean "show_gender_symbols", default: true, null: false
+    t.string "word_type_wording", default: "default", null: false
+    t.string "genus_wording", default: "default", null: false
     t.index ["owner_id"], name: "index_word_view_settings_on_owner_id"
     t.index ["theme_adjective_id"], name: "index_word_view_settings_on_theme_adjective_id"
     t.index ["theme_function_word_id"], name: "index_word_view_settings_on_theme_function_word_id"
