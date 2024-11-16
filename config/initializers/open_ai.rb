2114718135f29e317d97670799d32e687344f0b9
Serge Hänni <serge@nyi.ch>
2024-11-16 14:51:34 +0100

    Add default value for ollama URL

--- config/initializers/open_ai.rb
+++ config/initializers/open_ai.rb
@@ -2,6 +2,6 @@ Rails.application.config.to_prepare do
   OpenAI.configuration.uri_base = if Rails.env.test?
     "https://ai.test"
   else
-    ENV["OLLAMA_URL"]
+    ENV["OLLAMA_URL"].presence || "http://localhost:11434/v1"
   end
 end
