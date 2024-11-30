4fa98e82792af5236f6a5a21e22deab536f5bd6c
Serge Hänni <serge@nyi.ch>
2024-11-30 12:49:37 +0100

    Change `WordAttributeEdit#value` to a JSON parseable value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_24_130532) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_30_113628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pgcrypto"
