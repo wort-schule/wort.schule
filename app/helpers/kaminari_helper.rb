c085cb3e81e576309aa3fe61f20b8456485e73c9
Serge Hänni <serge@nyi.ch>
2025-01-15 22:04:47 +0100

    Change count label for related keywords
    [#616]

--- app/helpers/kaminari_helper.rb
+++ app/helpers/kaminari_helper.rb
@@ -3,6 +3,7 @@
 module KaminariHelper
   def paginate(scope, paginator_class: Kaminari::Helpers::Paginator, template: nil, **options)
     output = number_with_delimiter scope.total_count
+    model_name = options.delete(:model_name) || "activerecord.models.#{scope.model_name.i18n_key}"
 
     options[:total_pages] ||= scope.total_pages
     options.reverse_merge! current_page: scope.current_page, per_page: scope.limit_value, remote: false
@@ -11,7 +12,7 @@ module KaminariHelper
 
     content_tag :div, class: "text-xs text-gray-500 flex flex-col md:flex-row gap-2 md:items-center" do
       concat paginator.to_s
-      concat "#{output} #{scope.model_name.human(count: scope.total_count)}"
+      concat "#{output} #{I18n.t(model_name, count: scope.total_count)}"
     end
   end
 end
