0da1c5e948af3a41fb9332f264ef3db6b24eea44
Serge Hänni <serge@nyi.ch>
2025-03-23 14:49:53 +0100

    Add sorting of review attributes
    [#672]

--- app/models/change_group.rb
+++ app/models/change_group.rb
@@ -6,7 +6,25 @@ class ChangeGroup < ApplicationRecord
 
   enumerize :state, in: %i[waiting_for_review edited confirmed invalid created duplicate discarded], default: :waiting_for_review
 
-  has_many :word_attribute_edits, dependent: :destroy
+  has_many :word_attribute_edits,
+    # In Rails 8, replace with:
+    # in_order_of(:attribute_name, Llm::Enrich::ATTRIBUTE_GROUPS.flatten.map(&:to_s), filter: false)
+    lambda {
+      column = :attribute_name
+      values = Llm::Enrich::ATTRIBUTE_GROUPS.flatten.map(&:to_s)
+
+      model.disallow_raw_sql!([column], permit: model.adapter_class.column_name_with_order_matcher)
+      return spawn.none! if values.empty?
+
+      references = column_references([column])
+      self.references_values |= references unless references.empty?
+
+      values = values.map { |value| model.type_caster.type_cast_for_database(column, value) }
+      arel_column = column.is_a?(Arel::Nodes::SqlLiteral) ? column : order_column(column.to_s)
+
+      spawn.order!(build_case_for_value_position(arel_column, values))
+    },
+    dependent: :destroy
   has_one :new_word, dependent: :destroy
 
   accepts_nested_attributes_for :word_attribute_edits, :new_word
