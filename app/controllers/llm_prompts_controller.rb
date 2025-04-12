f8821df2d78c4c874cd1ddb2585e634fbb0b65e6
Serge Hänni <serge@nyi.ch>
2025-04-12 18:26:34 +0200

    Fix storing LLM prompts
    [#691]

--- app/controllers/llm_prompts_controller.rb
+++ app/controllers/llm_prompts_controller.rb
@@ -4,13 +4,14 @@ class LlmPromptsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    @llm_prompts = @llm_prompts.order(:identifier)
   end
 
   def edit
   end
 
   def update
-    if @llm_prompt.save
+    if @llm_prompt.update(llm_prompt_params)
       redirect_to llm_prompts_path
     else
       render :edit
