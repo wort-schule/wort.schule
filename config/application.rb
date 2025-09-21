85ef165b515f5004e6d20e463a2f4ecfeefa1a13
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-09-21 19:18:18 +0200

    Revert "Fix Rails autoloading error with mina/mise.rb"
    This reverts commit e3f7238ca50becfe8d775090cbc6eb0c3d69eaf7.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Wortschule
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets generators mina tasks templates])
+    config.autoload_lib(ignore: %w[assets generators tasks templates])
 
     # Configuration for the application, engines, and railties goes here.
     #
